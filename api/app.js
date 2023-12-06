09eafe6c3fd39c17a45d6d3b3ca806d6f554e3fc
Thiago Maran <thiagobiazusmaran@gmail.com>
2023-12-06 19:38:34 -0300

    att 06 12

--- api/app.js
+++ api/app.js
@@ -2,18 +2,17 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const app = express();
 const cors = require('cors');
 const multer = require('multer');
 
-const storage = multer.memoryStorage(); // Armazena a imagem na memória
-const upload = multer({ storage: storage });
-
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors());
+
+const upload = multer({ dest: '../uploads' });
 
 app.listen(8080, async () => {
   console.log('App iniciado');
@@ -26,28 +25,30 @@ app.listen(8080, async () => {
   }
 });
 
+//------------------------------------------
+
+
+
 const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize({ //troque para seu perfil
   dialect: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  username: 'postgres',
+  password: 'Thigasbm',
   database: 'CatImo',
 });
 
 
 //Definição das tabelas
-
-
 const Usuario = sequelize.define(
   'usuarios',
   {
     id_usuario: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true // Se transforma em serial automaticamente no postgres
+      autoIncrement: true
     },
     nome: { type: DataTypes.STRING, },
     sobrenome: { type: DataTypes.STRING, },
@@ -63,7 +64,7 @@ const Usuario = sequelize.define(
   },
   {
     timestamps: false,
-    paranoid: true, //utilizado quando uma tabela não será deletada OBS: O campo deverá ser timestamp
+    paranoid: true,
   }
 );
 
@@ -75,15 +76,14 @@ const Imovel = sequelize.define(
       primaryKey: true,
       autoIncrement: true
     },
-
-    imagens: {
-      type: DataTypes.BLOB,
-    },
-
+    imagens: { type: DataTypes.TEXT, },  
     tipo_operacao: { type: DataTypes.STRING, },
+    titulo: { type: DataTypes.STRING, },
+    razao: { type: DataTypes.STRING, },
+    id_usuario: { type: DataTypes.INTEGER, },
     zona: { type: DataTypes.STRING, },
     cidade: { type: DataTypes.STRING, },
-    uf_estado: { type: DataTypes.STRING, },
+    estado: { type: DataTypes.STRING, },
     cep: { type: DataTypes.STRING, },
     especie: { type: DataTypes.STRING, },
     valor: { type: DataTypes.STRING, },
@@ -97,112 +97,40 @@ const Imovel = sequelize.define(
   },
   {
     timestamps: false,
-    paranoid: true, //utilizado quando uma tabela não será deletada OBS: O campo deverá ser timestamp
+    paranoid: true,
   }
 );
 
-const Especificacao = sequelize.define(
-  'especificacoes',
+const Cidade = sequelize.define(
+  'cidades',
   {
-    id_espec: {
+    id_cidade: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
-    id_imovel: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'imoveis',
-        key: 'id_imovel'
-      }
-    },
-    descricao: { type: DataTypes.STRING },
-    medida: { type: DataTypes.STRING },
-    quantidade: { type: DataTypes.INTEGER }
+    nome: { type: DataTypes.STRING, },
+    uf_estado: { type: DataTypes.STRING, },
   },
   {
     timestamps: false,
-    paranoid: true
+    paranoid: true,
   }
 );
-
-Imovel.hasMany(Especificacao, { foreignKey: 'id_imovel' });
-Especificacao.belongsTo(Imovel, { foreignKey: 'id_imovel' });
-
-/*
-const ft_Espec = sequelize.define(
-  'fotos_espec',
-  {
-    id_foto: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    id_espec: { type: DataTypes.STRING },
-    descricao: { type: DataTypes.STRING },
-  },
-  {
-    timestamps: false,
-    paranoid: true,
-  }
-);*/
-/*
-const arq_Doc = sequelize.define(
-  'arquivos_doc',
-  {
-    id_arq: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    id_doc: { type: DataTypes.STRING },
-    descricao: { type: DataTypes.STRING },
-  },
-  {
-    timestamps: false,
-    paranoid: true,
-  }
-);*/
-/*
-const doc_Ope = sequelize.define(
-  'doc_ope',
-  {
-    id_doc: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    tipo_operacao: { type: DataTypes.STRING },
-    descricao: { type: DataTypes.STRING },
-  },
-  {
-    timestamps: false,
-    paranoid: true,
-  }
-);*/
-
 
 
 //Funções Usuario
 
-
-async function criarUsuario(req, res) {
+app.post('/usuarios', async (req, res) => {
   try {
-    if (!req.body.nome || !req.body.sobrenome || !req.body.email || !req.body.senha || !req.body.cpf || !req.body.cidade || !req.body.data_nasc || !req.body.rg || !req.body.cep || !req.body.telefone) {
-      return res.status(422).json({ msg: "Campos obrigatórios não foram preenchidos" });
-    }
-    console.log(req.body)
     const usuario = await Usuario.create(req.body);
     res.status(201).json(usuario);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao criar usuário' });
   }
+});
 
-
-}
-
-//funcionando
 async function login(req, res) {
   const { email, senha } = req.body;
   const usuario = await Usuario.findOne({ where: { email } });
@@ -214,7 +142,8 @@ async function login(req, res) {
   if (usuario.senha != req.body.senha) {
     return res.status(401).json({ mensagem: 'Email ou senha inválidos' });
   }
-  res.json({ token: jwt.sign(JSON.stringify(usuario), 'senha do token') })  // Gere e retorne um token JWT ou faça o que for necessário após a autenticação bem-sucedida.
+  res.json({ token: jwt.sign(JSON.stringify(usuario), 'senha do token'), id: usuario.id_usuario })
+
 }
 
 async function listarUsuarios(req, res) {
@@ -225,6 +154,18 @@ async function listarUsuarios(req, res) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao listar usuários' });
   }
+
+}
+
+async function listarUsuarioEsp(req, res) {
+  try {
+    const usuarios = await Usuario.findByPk(req.params.id_usuario);
+    res.json(usuarios);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao listar usuários' });
+  }
+
 }
 
 async function deletarUsuario(req, res) {
@@ -233,12 +174,13 @@ async function deletarUsuario(req, res) {
     if (!usuario) {
       return res.status(404).json({ error: 'Usuário não encontrado' });
     }
-    await usuario.destroy(); //o método destroy depende do 
+    await usuario.destroy();
     res.json({ mensagem: 'Usuário deletado com sucesso' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao deletar usuário' });
   }
+
 }
 
 async function atualizarUsuario(req, res) {
@@ -254,6 +196,7 @@ async function atualizarUsuario(req, res) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao atualizar usuário' });
   }
+
 }
 
 async function atualizarUsuarioParcialmente(req, res) {
@@ -270,70 +213,54 @@ async function atualizarUsuarioParcialmente(req, res) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao atualizar usuário' });
   }
+
 }
-
-app.post('/usuarios', async (req, res) => {
-  try {
-    const usuario = await Usuario.create(req.body);
-    res.status(201).json(usuario);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro ao criar usuário' });
-  }
-});
-
 
 //Funções Imovel
 
-
-async function criarImovel(req, res) {
+async function criarImovel(req, files, res) {
   try {
-    // Outros campos do formulário
-    const { tipo_operacao, zona, cidade, estado, especie, valor, bairro, rua, cep, numero, tamanho_terreno, tamanho_moradia, info_complementares } = req.body;
 
+const { tipo_operacao, titulo, razao, id_usuario, zona, cidade, estado, especie, valor, bairro, rua, cep, numero, complemento, tamanho_terreno, tamanho_moradia, info_complementares } = req;
 
+    const imagens = files.map(files => files.path);
+    console.log(imagens[0]);
 
+    let nome_imagem = imagens[0];
 
+ 
     const imovel = await Imovel.create({
       tipo_operacao,
+      titulo,
+      razao,
+      id_usuario,
       zona,
       cidade,
-      uf_estado: estado,
+      estado,
       cep,
       especie,
       valor,
       bairro,
       rua,
       numero,
-      complemento: req.body.complemento,
+      complemento,
       tamanho_terreno,
       tamanho_moradia,
       info_complementares,
-      imagens: req.file.buffer, // Armazena a imagem binária no banco de dados
+      imagens: nome_imagem,
     });
 
+    console.log('Imóvel criado com sucesso:', imovel);
 
-    // Process room specifications
-    const roomSpecs = req.body.roomSpecs; // Assuming you have an array of room specifications in the request body
-    if (roomSpecs && roomSpecs.length > 0) {
-      for (const roomSpec of roomSpecs) {
-        const { descricao, medida, quantidade } = roomSpec;
-        await Especificacao.create({
-          id_imovel: imovel.id_imovel, // Assign the Imovel ID as the foreign key
-          descricao,
-          medida,
-          quantidade
-        });
-      }
-    }
+    return res.status(201).json({ success: true, imovel });
 
-    await imovel.save();
-    res.status(201).json({ msg: 'Imóvel criado com sucesso' });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro ao criar imóvel' });
+    console.error('Erro ao criar imóvel:', error);
+    //return res.status(500).json({ error: 'Erro interno no servidor.' });
   }
-}
+
+  }
+
 
 async function listarImoveis(req, res) {
   try {
@@ -343,10 +270,34 @@ async function listarImoveis(req, res) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao listar imóveis' });
   }
+
+}
+
+async function listarImovelEsp(req, res) {
+  try {
+    const imovel = await Imovel.findByPk(req.params.id_imovel);
+    res.json(imovel);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao listar usuários' });
+  }
+
+}
+
+//listar cidades
+async function listarCidades(req, res) {
+  try {
+    const cidade = await Cidade.findAll();
+    res.json(cidade);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao listar imóveis' });
+  }
+
 }
 
 //usuarios
-app.post('/usuario', criarUsuario);
+app.get('/usuario/:id_usuario', listarUsuarioEsp)
 app.post('/login', login);
 app.get('/usuarios', listarUsuarios);
 app.delete('/usuarios/:id_usuario', deletarUsuario);
@@ -354,5 +305,21 @@ app.put('/usuarios/:id_usuario', atualizarUsuario);
 app.patch('/usuarios/:id_usuario', atualizarUsuarioParcialmente);
 
 //imoveis
-app.post('/imovel', upload.single('imagens'), criarImovel);;
+app.post('/imovel', upload.array('imagens', 10), (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+
+  const { body, files } = req;  // Extrair body e files do req
+
+  criarImovel(body, files, res)
+    .catch(error => {
+      console.error(error);
+      // res.status(500).json({ error: 'Erro ao processar a solicitação' });
+    });
+});
+
+app.get('/imovel/:id_imovel', listarImovelEsp);
 app.get('/imoveis', listarImoveis);
+
+//cidade
+app.get('/cidades', listarCidades)
